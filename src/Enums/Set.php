55bc5959f8392b8b55e5bfa25e98332f7ff8b3f6
Mehmet Uyanik <mehmet.uyanik@live.com.au>
2025-03-30 09:41:59 +1100

    Add more sets.

--- src/Enums/Set.php
+++ src/Enums/Set.php
@@ -4,21 +4,23 @@ namespace Memuya\Fab\Enums;
 
 enum Set: string
 {
-    case WelcomeToRathe = 'WTR';
-    case ArcaneRising = 'ARC';
-    case CrusibleOfWar = 'CRU';
-    case TalesOfAria = 'ELE';
-    case Monarch = 'MON';
-    case Everfest = 'EVR';
-    case HistoryPackOne = '1HP';
-    case Uprising = 'UPR';
-    case Dynasty = 'DYN';
-    case Outsiders = 'OUT';
-    case DuskTillDawn = 'DTD';
-    case BrightLights = 'EVO';
-    case RoundTheTable = 'TCC';
-    case HeavyHitters = 'HVY';
-    case PartTheMistveil = 'MST';
-    case Rosetta = 'ROS';
-    case TheHunted = 'HNT';
+    case WelcomeToRathe = 'WTR'; // 2019
+    case ArcaneRising = 'ARC'; // 2020
+    case CrusibleOfWar = 'CRU'; // 2020
+    case Monarch = 'MON'; // 2021
+    case TalesOfAria = 'ELE'; // 2021
+    case Everfest = 'EVR'; // 2022
+    case HistoryPackOne = '1HP'; // 2022
+    case Uprising = 'UPR'; // 2022
+    case Dynasty = 'DYN'; // 2022
+    case ClassicBattlesRhinarVsDorinthea = 'CBR'; // 2022
+    case Outsiders = 'OUT'; // 2023
+    case HistoryPackTwo = 'HP2'; // 2023
+    case DuskTillDawn = 'DTD'; // 2023
+    case BrightLights = 'EVO'; // 2023
+    case RoundTheTable = 'TCC'; // 2023
+    case HeavyHitters = 'HVY'; // 2024
+    case PartTheMistveil = 'MST'; // 2024
+    case Rosetta = 'ROS'; // 2024
+    case TheHunted = 'HNT'; // 2024
 }
