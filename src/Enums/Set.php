274a5c27f7aef7df26ad89b90cc62b37292dd14b
Mehmet Uyanik <mehmet.uyanik@live.com.au>
2025-04-20 16:33:45 +1000

    Added more sets.

--- src/Enums/Set.php
+++ src/Enums/Set.php
@@ -4,24 +4,70 @@ namespace Memuya\Fab\Enums;
 
 enum Set: string
 {
+    // Booster Sets
     case WelcomeToRathe = 'WTR'; // 2019
     case ArcaneRising = 'ARC'; // 2020
-    case CrusibleOfWar = 'CRU'; // 2020
+    case CrucibleOfWar = 'CRU'; // 2020
     case Monarch = 'MON'; // 2021
     case TalesOfAria = 'ELE'; // 2021
     case Everfest = 'EVR'; // 2022
-    case HistoryPackOne = '1HP'; // 2022
     case Uprising = 'UPR'; // 2022
     case Dynasty = 'DYN'; // 2022
-    case ClassicBattlesRhinarVsDorinthea = 'CBR'; // 2022
     case Outsiders = 'OUT'; // 2023
-    case HistoryPackTwo = 'HP2'; // 2023
     case DuskTillDawn = 'DTD'; // 2023
     case BrightLights = 'EVO'; // 2023
-    case RoundTheTable = 'TCC'; // 2023
     case HeavyHitters = 'HVY'; // 2024
     case PartTheMistveil = 'MST'; // 2024
     case Rosetta = 'ROS'; // 2024
-    case TheHunted = 'HNT'; // 2024
+    case TheHunted = 'HNT'; // 2025
     case HighSeas = 'SEA'; // 2025
+
+    // Blitz Decks
+    case PrismBlitzDeck = 'PSM'; // Monarch Blitz Deck - Prism
+    case BoltynBlitzDeck = 'BLT'; // Monarch Blitz Deck - Boltyn
+    case ChaneBlitzDeck = 'CHN'; // Monarch Blitz Deck - Chane
+    case LeviaBlitzDeck = 'LEV'; // Monarch Blitz Deck - Levia
+    case BriarBlitzDeck = 'BRI'; // Tales of Aria Blitz Deck - Briar
+    case LexiBlitzDeck = 'LXI'; // Tales of Aria Blitz Deck - Lexi
+    case OldhimBlitzDeck = 'OLD'; // Tales of Aria Blitz Deck - Oldhim
+    case FaiBlitzDeck = 'FAI'; // Uprising Blitz Deck - Fai
+    case DromaiBlitzDeck = 'DRM'; // Uprising Blitz Deck - Dromai
+    case IyslanderBlitzDeck = 'IYS'; // Uprising Blitz Deck - Iyslander
+    case ArakniBlitzDeck = 'ARA'; // Outsiders Blitz Deck - Arakni
+    case UzuriBlitzDeck = 'UZU'; // Outsiders Blitz Deck - Uzuri
+    case RiptideBlitzDeck = 'RIP'; // Outsiders Blitz Deck - Riptide
+    case AzaleaBlitzDeck = 'AZL'; // Outsiders Blitz Deck - Azalea
+    case KatsuBlitzDeck = 'KAT'; // Outsiders Blitz Deck - Katsu
+    case BenjiBlitzDeck = 'BNJ'; // Outsiders Blitz Deck - Benji
+    case ViseraiBlitzDeck = 'VIS'; // History Pack 1 Blitz Deck - Viserai
+    case DorintheaBlitzDeck = 'DOR'; // History Pack 1 Blitz Deck - Dorinthea
+    case RhinarBlitzDeck = 'RHI'; // History Pack 1 Blitz Deck - Rhinar
+    case DashBlitzDeck = 'DAS'; // History Pack 1 Blitz Deck - Dash
+    case BravoBlitzDeck = 'BRV'; // History Pack 1 Blitz Deck - Bravo
+    case KanoBlitzDeck = 'KNO'; // History Pack 1 Blitz Deck - Kano
+    case MelodyBlitzDeck = 'MEL'; // Round the Table Blitz Deck - Melody
+    case BrevantBlitzDeck = 'BRE'; // Round the Table Blitz Deck - Brevant
+    case ProfessorBlitzDeck = 'PRO'; // Round the Table Blitz Deck - The Professor
+    case IraBlitzDeck = 'IRA'; // Welcome Deck - Ira
+    case BravoWelcomeDeck = 'BRW'; // Welcome Deck - Bravo
+    case AuroraFirstStrikeBlitzDeck = 'AFS'; // 1st Strike Blitz Deck - Aurora
+    case TerraFirstStrikeBlitzDeck = 'TFS'; // 1st Strike Blitz Deck - Terra
+
+    // Classic Battles
+    case ClassicBattlesRhinarVsDorinthea = 'CBR'; // 2022
+
+    // History Packs
+    case HistoryPackOne = '1HP'; // 2022
+    case HistoryPackTwo = '2HP'; // 2023
+
+    // Armory Decks (Classic Constructed)
+    case ArmoryDeckAurora = 'ADA'; // 2024
+    case ArmoryDeckJarlVetreidi = 'ADJ'; // 2024
+    case ArmoryDeckDash = 'ADD'; // 2024
+    case ArmoryDeckKayo = 'ADK'; // 2024
+    case ArmoryDeckAzalea = 'ADZ'; // 2024
+    case ArmoryDeckBoltyn = 'ADB'; // 2024
+
+    // Multiplayer Products
+    case RoundTheTable = 'TCC'; // 2023
 }
